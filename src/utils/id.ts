9429f3fb2f901d0c73780745fae51d3d316037e3
Gustavo3142 <gustavo@singularabach.com>
2026-06-10 16:38:34 -0300

    Fix geração única de ID

--- src/utils/id.ts
+++ src/utils/id.ts
@@ -1,4 +1,4 @@
-export const generateId = () => {
+export const generateUniqueId = () => {
   // Retorna o milissegundo atual + 5 dígitos aleatórios, impossibilitando colisões
   return parseInt(`${Date.now()}${Math.floor(Math.random() * 90000 + 10000)}`);
 };
